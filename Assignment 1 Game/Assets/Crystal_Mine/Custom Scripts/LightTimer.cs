7702513ac3a3ba4cae44073df321f8ee4216592f
JamesAvenell <bernie.avenell@yahoo.com.au>
2018-10-01 13:05:40 +1000

    Added Scene Mine level
    Added Scene Mine
    Added Package Spider web and Water Drop
    Added Package Crystal Mine Custom Prefabs and Custom Scripts
    Custom Scripts contains jscript and C# light dimming script - torch will currently fade out after user preset time (30sec)

---
+++ Assignment 1 Game/Assets/Crystal_Mine/Custom Scripts/LightTimer.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class LightTimer : MonoBehaviour
+{
+    public Light lightToDim = null;
+    public float maxTime = 1; //30 seconds
+    public float mEndTime = 0;
+    public float mStartTime = 0;
+
+
+    void Awake()
+    {
+        mStartTime = Time.time;
+        mEndTime = mStartTime + maxTime;
+         //mEndTime = Time.time;
+         //mStartTime = mEndTime + maxTime;
+    }
+
+
+
+    void Update()
+    {
+        if (lightToDim)
+            lightToDim.intensity = Mathf.InverseLerp(mEndTime, mStartTime, Time.time);
+    }
+
+}
