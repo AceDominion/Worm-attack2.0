ce1a421c94a2db789bb0d0ac77e75b98f78a1343
eee61 <42558755+eee61@users.noreply.github.com>
2018-10-12 12:09:08 +1100

    Completion of minimap & webs
    Minimap:
    1. laid on the bottom left of the screen
    2. Maximize/minimize it with the key "M"
    
    Webs:
    1. Create a new prefab for spidewebs
    1. Web will disappear when the player touches it

---
+++ Assignment 1 Game/Assets/MapMove.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MapMove : MonoBehaviour {
+
+    public Transform target;
+    public Camera cam;
+    public GameObject map;
+    public bool FollowByTorch;
+
+    bool flagMaxMap = false;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (!flagMaxMap && Input.GetKeyDown(KeyCode.M))
+        {
+            var mapTransform = map.transform as RectTransform;
+            mapTransform.sizeDelta = new Vector2(Screen.width, Screen.height);
+            flagMaxMap = true;
+        }
+        else if (flagMaxMap && Input.GetKeyDown(KeyCode.M))
+        {
+            var mapTransform = map.transform as RectTransform;
+            mapTransform.sizeDelta = new Vector2(180, 180);
+            flagMaxMap = false;
+        }
+
+        
+	}
+
+    private void OnGUI()
+    {
+        if (FollowByTorch)
+            transform.position = cam.WorldToScreenPoint(new Vector3(target.position.x + 1, target.position.y - 1)); //also need to reset the anchor 
+
+        // transform.position = new Vector3(cam.WorldToScreenPoint(target.transform.position).x + 300, cam.WorldToScreenPoint(target.transform.position).y+300) ;
+
+    }
+}
--- Assignment 1 Game/Assets/WebTransparency.cs
+++ Assignment 1 Game/Assets/WebTransparency.cs
@@ -2,15 +2,17 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class WebTransparency : MonoBehaviour {
+public class WebTransparency : MonoBehaviour
+{
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    /**/
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.transform.name.Equals("RigidBodyFPSController"))
+        {
+            Destroy(gameObject);
+        }
+    }
+
+
 }
