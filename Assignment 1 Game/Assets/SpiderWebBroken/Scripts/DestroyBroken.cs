d01e4bd1710f627cd5bcb7faf625609e0d99e602
JamesAvenell <bernie.avenell@yahoo.com.au>
2018-10-15 09:10:48 +1100

    Added Mine Scene
    Added Mine Scene to master branch
    Contains web destroy script on mouse click within certain range
    Water drop animations added

---
+++ Assignment 1 Game/Assets/SpiderWebBroken/Scripts/DestroyBroken.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyBroken : MonoBehaviour {
+
+
+	
+	// Update is called once per frame
+	void Update () {
+
+        Destroy(gameObject, 2f);
+
+    }
+}
---
+++ Assignment 1 Game/Assets/SpiderWebBroken/Scripts/WebDestry.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WebDestry : MonoBehaviour
+{
+
+    // Use this for initialization
+
+    public GameObject remains; // this is the prefab, assigned manually in the editor
+    public GameObject theBoy;
+    public GameObject theWeb;
+
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+            if (Input.GetMouseButtonDown(0) & theBoy.transform.position.x < theWeb.transform.position.x + 2f)
+            {
+                Instantiate(remains, gameObject.transform.position, gameObject.transform.rotation);
+                Destroy(gameObject);
+                Destroy(remains, 2.0f);
+            }
+        
+
+    }
+
+}
