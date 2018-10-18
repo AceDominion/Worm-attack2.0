23d20c6e3b5cef9efeeb2200adecacd1ae6abd29
Liam <liamkenna@live.com.au>
2018-10-18 11:44:20 +1100

    Added totems
    Still requires script for totem pickup

---
+++ Assignment 1 Game/Assets/TotemRotate.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TotemRotate : MonoBehaviour {
+    int height = 0; //range from minus to positive, controls "bobbing"
+    bool up; //true = going up, false = down.
+    public int speed;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.Rotate(Vector3.up * (Time.deltaTime* speed));
+
+        if (up) //neg
+        {
+            transform.Translate(Vector3.up / 400);
+            //transform.position = Vector3.up * Time.deltaTime;
+            //transform.position = new Vector3(0.0f, 2f);
+            height++;
+        }
+        else//pos
+        {
+            transform.Translate(-1*(Vector3.up / 400));
+            //transform.position = (Vector3.up * Time.deltaTime)*-1;
+            //transform.position = new Vector3(0.0f, -2f);
+            height--;            
+        }
+        if (height == 40)
+        {
+            up = false;
+        }
+        if(height == -40)
+        {
+            up = true;
+        }
+
+	}
+}
