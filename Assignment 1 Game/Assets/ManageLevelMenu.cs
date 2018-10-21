772a1a1fde6f38bb914dc2a53a54bdb45f05b3dc
Liam <liamkenna@live.com.au>
2018-10-21 20:05:18 +1100

    level selection

--- Assignment 1 Game/Assets/ManageLevelMenu.cs
+++ Assignment 1 Game/Assets/ManageLevelMenu.cs
@@ -1,16 +1,54 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class ManageLevelMenu : MonoBehaviour {
+    public Button b1;
+    public Button b2;
+    string level = "aztec";
 
 	// Use this for initialization
 	void Start () {
-		
-	}
+        Button left = b1.GetComponent<Button>();
+        Button right = b2.GetComponent<Button>();
+
+        left.onClick.AddListener(rotater);
+        right.onClick.AddListener(rotater);
+    }
 	
 	// Update is called once per frame
 	void Update () {
-		
+		//sets up level select, shows aztec picture
+        if(level == "Aztec")
+        {
+
+        }
 	}
+
+    void rotater()
+    {
+        if (level == "aztec")
+        {
+            level = "cave";
+            Debug.Log("Cave");
+        }
+        else
+        {
+            level = "aztec";
+            Debug.Log("Aztec");
+        }
+    }
+    void clear(bool aztec)
+    {
+        if (aztec)
+        {
+
+        }
+        else
+        {
+
+        }
+    }
+    
 }
