1bf8038408d020fc20cf059f3af7238749bea565
JamesAvenell <bernie.avenell@yahoo.com.au>
2018-10-21 14:32:31 +1100

    Mayn Assets for Mine cauldrons and llight switch script

---
+++ Assignment 1 Game/Assets/Crystal_Mine/Custom Scripts/TurnLight.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TurnLight : MonoBehaviour
+{
+
+    public GameObject light;
+    //public Light lightToDim = null;
+    private bool on = false;
+
+    // Use this for initialization
+    void OnTriggerStay(Collider plyr)
+    {
+        if (plyr.tag == "Player" && Input.GetMouseButtonDown(0) && !on)
+        {
+            light.SetActive(true);
+            on = true;
+
+        }
+        else if (plyr.tag == "Player" && Input.GetMouseButtonDown(0) && on)
+        {
+            light.SetActive(false);
+            on = false;
+        }
+
+
+
+    }
+}
+
