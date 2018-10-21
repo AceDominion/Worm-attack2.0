ef36303683a2f7cdf3ddf9a2b65dcae3ff461c6e
Liam <liamkenna@live.com.au>
2018-10-21 16:39:17 +1100

    Revert "Added Effie scenes"
    This reverts commit 2196d957ff995160a1a6e067ec17f385330c8a20.

--- Assignment 1 Game/Assets/TotemRotate.cs
+++ Assignment 1 Game/Assets/TotemRotate.cs
@@ -6,35 +6,28 @@ public class TotemRotate : MonoBehaviour {
     int height = 0; //range from minus to positive, controls "bobbing"
     bool up; //true = going up, false = down.
     public int speed;
-    public bool button;
-    bool hover; //mouse over.
 	// Use this for initialization
 	void Start () {
-		if (!button)
-        {
-            hover = true;
-        }
+		
 	}
 	
 	// Update is called once per frame
 	void Update () {
         transform.Rotate(Vector3.up * (Time.deltaTime* speed));
-        if (hover)
+
+        if (up) //neg
         {
-            if (up) //neg
-            {
-                transform.Translate(Vector3.up / 400);
-                //transform.position = Vector3.up * Time.deltaTime;
-                //transform.position = new Vector3(0.0f, 2f);
-                height++;
-            }
-            else//pos
-            {
-                transform.Translate(-1 * (Vector3.up / 400));
-                //transform.position = (Vector3.up * Time.deltaTime)*-1;
-                //transform.position = new Vector3(0.0f, -2f);
-                height--;
-            }
+            transform.Translate(Vector3.up / 400);
+            //transform.position = Vector3.up * Time.deltaTime;
+            //transform.position = new Vector3(0.0f, 2f);
+            height++;
+        }
+        else//pos
+        {
+            transform.Translate(-1*(Vector3.up / 400));
+            //transform.position = (Vector3.up * Time.deltaTime)*-1;
+            //transform.position = new Vector3(0.0f, -2f);
+            height--;            
         }
         if (height == 40)
         {
@@ -46,21 +39,4 @@ public class TotemRotate : MonoBehaviour {
         }
 
 	}
-
-    private void OnMouseOver()
-    {
-        if (button)
-        {
-            Debug.Log("on");
-            hover = true;
-        }
-    }
-    private void OnMouseExit()
-    {
-        if (button)
-        {
-            Debug.Log("off");
-            hover = false;
-        }
-    }
 }
