f923a4e174695698a5a8dddd02b4fdfbff1f9ade
AceDominion <acedominion1@gmail.com>
2018-10-23 16:15:29 +1100

    Menu stuff

--- Assignment 1 Game/Assets/Scenes/Title/SceneSwitcher.cs
+++ Assignment 1 Game/Assets/Scenes/Title/SceneSwitcher.cs
@@ -7,19 +7,9 @@ public class SceneSwitcher : MonoBehaviour {
 
 	public void SwitchScene(string sceneName)
     {
-        //play
-        //自动进入最后一个level 
-
-        //level
-        //进入level scene
-
-        //credit
-        //展现 name 本次共收集到的totem
+        
         SceneManager.LoadScene (sceneName);
 
-        Debug.Log (SceneManager.GetSceneByName ("Title").isLoaded);
-        if (SceneManager.GetSceneByName ("Title").isLoaded == false) {
-           
-        }
+
     }
 }
