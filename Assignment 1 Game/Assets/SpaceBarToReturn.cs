1e679a3afdac2c068d4cdb048fd93f7cd7585274
Ken-na <43331896+Ken-na@users.noreply.github.com>
2018-10-23 18:46:56 +1100

    finale

---
+++ Assignment 1 Game/Assets/SpaceBarToReturn.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SpaceBarToReturn : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKeyDown("e"))
+        {
+            Cursor.visible = true;
+            Debug.Log("MOUSE VIS");
+        }
+        if (Input.GetKeyDown("1"))
+        {
+            SceneManager.LoadScene("Levels");
+        }
+        if (Input.GetKeyDown("2"))
+        {
+            SceneManager.LoadScene("map one");
+        }
+        if (Input.GetKeyDown("3"))
+        {
+            SceneManager.LoadScene("Mine");
+        }
+        if (Input.GetKeyDown("space"))
+        {
+            if (SceneManager.GetSceneByName("title").isLoaded == false)
+            {
+                SceneManager.LoadScene("Title");
+            }
+        }
+	}
+
+}
